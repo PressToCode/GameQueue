b20ae9e3c6d124408f68e573c6f37185faf3a107
olfat <code.olfatfaiz@gmail.com>
2025-05-31 22:15:49 +0700

    - Added Functional Fragments for Bottom NavBar - Added an ugly homepage with functional collapsible scrolling behavior

--- app/src/main/java/com/example/gamequeue/ui/main/MainActivity.java
+++ app/src/main/java/com/example/gamequeue/ui/main/MainActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.gamequeue.R;
+import com.example.gamequeue.ui.fragment.HomeFragment;
+import com.example.gamequeue.ui.fragment.ReservationFragment;
+import com.example.gamequeue.ui.fragment.StatusFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     // Variables
     private BottomNavigationView bottomNavigationView;
     private FloatingActionButton fabReservasi;
+    private final int NAV_HOME = R.id.nav_home;
+    private final int NAV_RESERVATION = R.id.nav_reservation;
+    private final int NAV_STATUS = R.id.nav_status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +34,38 @@ public class MainActivity extends AppCompatActivity {
         fabReservasi = findViewById(R.id.fab_reservasi);
 
         // Setup all fragments and listener
-//        setup();
+        setup();
     }
 
     private void setup() {
+        // Set Default Fragment
+        loadFragment(new HomeFragment());
+
+        // Set FAB Button Listener
         fabReservasi.setOnClickListener(v -> {
-            bottomNavigationView.setSelectedItemId(R.id.nav_reservation);
+            bottomNavigationView.setSelectedItemId(NAV_RESERVATION);
+            loadFragment(new HomeFragment());
         });
+
+        // Set Bottom Navigation View Listener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == NAV_HOME) {
+                loadFragment(new HomeFragment());
+                return true;
+            } else if(item.getItemId() == NAV_RESERVATION) {
+                loadFragment(new ReservationFragment());
+                return true;
+            } else if(item.getItemId() == NAV_STATUS) {
+                loadFragment(new StatusFragment());
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
