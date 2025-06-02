503d052f6e825d3939da3356688c389a355beaf7
olfat <code.olfatfaiz@gmail.com>
2025-06-03 00:56:05 +0700

    - Added 3rd Party Library for Step Indicator - Added Reservation Process rough layout

---
+++ app/src/main/java/com/example/gamequeue/ui/main/ReservationProcessActivity.java
@@ -0,0 +1,26 @@
+package com.example.gamequeue.ui.main;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gamequeue.R;
+
+public class ReservationProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_reservation_process);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
