0c509898635892f6764cae55d3f1454becc444d5
Natha Satvika <nathasatvika@gmail.com>
2025-06-03 14:14:53 +0700

    feat: add RegisterActivity

--- app/src/main/java/com/example/gamequeue/ui/main/LoginActivity.java
+++ app/src/main/java/com/example/gamequeue/ui/main/LoginActivity.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import com.example.gamequeue.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,16 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivty_login);
-
-        // Mencari tombol login di layout
         Button login = findViewById(R.id.btn_login);
 
-        // Menambahkan event listener untuk tombol login
         login.setOnClickListener(v -> {
-            // Mengarahkan pengguna ke MainActivity setelah tombol login diklik
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
-            finish(); // Menutup LoginActivity agar tidak bisa kembali ke halaman login
+            finish();
         });
     }
 }
---
+++ app/src/main/java/com/example/gamequeue/ui/main/RegisterActivity.java
@@ -0,0 +1,4 @@
+package com.example.gamequeue.ui.main;
+
+public class RegisterActivity {
+}
